cb56ebfd27e86347ed904a8b067f64a2d6a9e7b3
ScaryLewii <thumeo1091@gmail.com>
2023-03-06 10:53:31 +0700

    using initialprops

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from '@/components/layout/layout'
+import { getServiceList, getStaffList } from '@/firebase/functions'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
@@ -7,4 +8,11 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
     </Layout>
   )
+}
+
+App.getInitialProps = async ctx => {
+  const staffsData = await getStaffList()
+  const servicesData = await getServiceList()
+
+	return { staffs: staffsData, services: servicesData }
 }
